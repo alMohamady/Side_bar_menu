0747e6d03742bbf3526a83b91034afdba213c316
al Mohamady <al.Mohamady47@gmail.com>
2020-02-11 16:44:18 +0200

    Second session

--- lib/classes/collapsing_list_tile.dart
+++ lib/classes/collapsing_list_tile.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../theme.dart';
+
 
 class CollapsingListTile extends StatefulWidget{
 
   final String title;
   final IconData icon;
+  final AnimationController animationController;
 
-
-  CollapsingListTile({@required this.title,@required this.icon});
+  CollapsingListTile({@required this.title,@required this.icon, @required this.animationController});
 
   @override
   State<StatefulWidget> createState() {
@@ -17,17 +19,29 @@ class CollapsingListTile extends StatefulWidget{
 }
 
 class CollapsingListTileState extends State<CollapsingListTile>{
+
+  Animation<double> _widthAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _widthAnimation = Tween <double> (begin: 250, end: 65).animate(widget.animationController);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Row(
-      children: <Widget>[
-        Icon(widget.icon, color: Colors.white30,),
-        SizedBox(width: 10,),
-        Text(widget.title)
-      ],
+    return Container(
+      width: _widthAnimation.value,
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: Row(
+        children: <Widget>[
+          Icon(widget.icon, color: Colors.white30,),
+          SizedBox(width: 10,),
+          (_widthAnimation.value >= 220) ? Text(widget.title, style: listTitleDefaultTextStyle) : Container()
 
-
+        ],
+      ),
     );
   }
 }
--- lib/classes/navigtion_drawer.dart
+++ lib/classes/navigtion_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_side_menu/models/navigation_model.dart';
+import 'package:flutter_app_side_menu/theme.dart';
 
 import 'collapsing_list_tile.dart';
 
@@ -12,20 +13,68 @@ class NavigationDrawer extends StatefulWidget{
   }
 }
 
-class NavigationDrawerState extends State<NavigationDrawer>{
+class NavigationDrawerState extends State<NavigationDrawer> with SingleTickerProviderStateMixin {
+  double maxWidth = 250;
+  double minWidth = 65;
+  bool isCollapsed = false;
+  AnimationController _animationController;
+  Animation<double> widthAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    widthAnimation = Tween<double>(begin: maxWidth, end: minWidth).animate(
+        _animationController);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, widget) {
+        return getWidget(context, widget);
+      },
+    );
+  }
+
+  Widget getWidget(context, widget) {
     return Container(
-      width: 250,
+      width: widthAnimation.value,
+      color: drawerBackgroundColor,
       child: Column(
         children: <Widget>[
-          ListView.builder(itemBuilder: (context, counter) {
-            return CollapsingListTile(
-              title: navigationItems[counter].title,
-              icon: navigationItems[counter].icon
-            );
-          })
+          SizedBox(height: 50,),
+          CollapsingListTile(
+            title: 'Ahmed Mohamady',
+            icon: Icons.person,
+            animationController: _animationController,
+          ),
+          Expanded(
+            child: ListView.builder(itemBuilder: (context, counter) {
+              return CollapsingListTile(
+                title: navigationItems[counter].title,
+                icon: navigationItems[counter].icon,
+                animationController: _animationController,
+              );
+            },
+              itemCount: navigationItems.length,
+            ),
+          ),
+          InkWell(
+              onTap: () {
+                setState(() {
+                  isCollapsed = !isCollapsed;
+                  isCollapsed
+                      ? _animationController.forward()
+                      : _animationController.reverse();
+                });
+              },
+              child: Icon(Icons.chevron_left, color: Colors.white, size: 50,)
+          ),
+          SizedBox(height: 50,),
         ],
       ),
     );
